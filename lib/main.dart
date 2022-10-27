90e4fa8a85d6e34b35fb81c0a5633d50e20595a1
AHEYOND <aheyond@gmail.com>
2022-10-26 21:38:13 -0500

    Agrega cambios mas recientes de la aplicación

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/services.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
