acd1a5aced6d1cf9c49e9a7d42ab67eaf47dc0c9
fwcheesecake <antoniore102001@gmail.com>
2022-11-02 16:57:12 -0600

    Reformat code

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -5,44 +5,44 @@ import 'package:flutter/material.dart';
 
 import 'estadisticas.dart';
 
-class SplashScreen extends StatefulWidget{
+class SplashScreen extends StatefulWidget {
   SplashScreen({Key? key}) : super(key: key);
 
-  @override 
+  @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    
     super.initState();
-    Timer(Duration(seconds:3), ()=>Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>EstadisticasScreen())));
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => EstadisticasScreen())));
   }
+
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
-       decoration: const BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
           colors: <Color>[
             Color(0xFF12B04E),
             Color(0xFF6DD5DE),
-            
           ],
         ),
       ),
-      child: Stack(
-      children: <Widget>[
+      child: Stack(children: <Widget>[
         Center(
-          child: Image.asset("assets/images/Logo.png",
-          height: 100,
-          
-      ),
+          child: Image.asset(
+            "assets/images/Logo.png",
+            height: 100,
           ),
-      ]
-      ),
+        ),
+      ]),
     );
   }
 }
